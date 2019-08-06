be8ae080c47fb655316cfddab0df89b14f515fd8
r4nx <mod34@yandex.ru>
2019-08-07 01:01:15 +0300

    Grab settings from environment rather than config

--- settings.py
+++ settings.py
@@ -1,5 +1,8 @@
+import os
+
+
 SETTINGS = {
-    'BOT_TOKEN': 'yourtokenhere',
-    'LOCALE': 'en',
-    'CMD_PREFIX': '!'
+    'BOT_TOKEN': os.getenv('BHBOT_TOKEN', ''),
+    'LOCALE': os.getenv('BHBOT_LOCALE', 'ru'),
+    'CMD_PREFIX': os.getenv('BHBOT_CMD_PREFIX', '!')
 }
